0030d4913b5dab36c68932d2b575363f358416c3
nys3b8tvbsc <nys3b8tvbsc@yandex.ru>
2020-07-30 15:04:56 +0300

    Add PostgreSQL

--- rest_api/cah/settings.py
+++ rest_api/cah/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'cah.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'password',
+        'HOST': 'db',
+        'PORT': 5432
     }
 }
 
---
+++ rest_api/docker-entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sleep 5
+
+python manage.py makemigrations --noinput
+
+python manage.py migrate --noinput
+
+python manage.py runserver 0.0.0.0:8000
